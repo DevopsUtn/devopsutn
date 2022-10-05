eeb53658110b5aaa77d298f287a4f5c3db02c64f
Matias Benditti <matias.benditti@neoris.com>
2022-10-05 13:07:05 -0300

    Format date

--- src/main/java/com/utn/devops/models/Player.java
+++ src/main/java/com/utn/devops/models/Player.java
@@ -1,5 +1,6 @@
 package com.utn.devops.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,5 +10,6 @@ public class Player {
     Integer id;
     String firstName;
     String lastName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/YYYY")
     Date birthday;
 }
